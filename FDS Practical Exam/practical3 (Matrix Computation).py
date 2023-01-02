6ced526a8673126321aa8339dd4c6669600a869c
Vaibhav Shamrao Pawar <73544511+vaibhav-developer@users.noreply.github.com>
2023-01-02 17:31:53 +0530

    Add files via upload

---
+++ FDS Practical Exam/practical3 (Matrix Computation).py
@@ -0,0 +1,25 @@
+'''
+1.Write a Python program to compute following computation on matrix:
+a) Addition of two matrices   
+b) Subtraction of two matrices
+c) Multiplication of two matrices
+d) Transpose of a matrix
+'''
+
+import numpy
+
+
+x = numpy.array([[1, 2], [4, 5]])
+y = numpy.array([[7, 8], [9, 10]])
+
+print("The element wise addition of matrix is : ")
+print(numpy.add(x, y))
+
+print("The element wise subtraction of matrix is : ")
+print(numpy.subtract(x, y))
+
+print ("The product of matrices is : ")
+print (numpy.dot(x,y))
+
+print("The transpose of given matrix is : ")
+print(x.T)
